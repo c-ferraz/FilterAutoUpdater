4f0332897abd73ac9afae7f5fd086698356ee172
c-ferraz <64041496+c-ferraz@users.noreply.github.com>
2020-06-24 03:02:56 -0300

    1.1.0
    Added support for Filterblade downloaded filters.
    Added backup of old filters.

--- FilterUpdater.py
+++ FilterUpdater.py
@@ -1,4 +1,4 @@
-#VERSON 1.0.3
+#VERSON 1.1.0
 #BY C. FERRAZ
 #https://github.com/c-ferraz
 
@@ -7,6 +7,8 @@ import os,sys
 import re
 import getpass
 import easygui
+import shutil
+from datetime import datetime
 
 url_soft = r"https://raw.githubusercontent.com/NeverSinkDev/NeverSink-Filter/master/NeverSink's%20filter%20-%200-SOFT.filter"
 url_regular = r"https://raw.githubusercontent.com/NeverSinkDev/NeverSink-Filter/master/NeverSink's%20filter%20-%201-REGULAR.filter"
@@ -15,6 +17,7 @@ url_strict = r"https://raw.githubusercontent.com/NeverSinkDev/NeverSink-Filter/m
 url_very_strict = r"https://raw.githubusercontent.com/NeverSinkDev/NeverSink-Filter/master/NeverSink's%20filter%20-%204-VERY-STRICT.filter"
 url_uber_strict = r"https://raw.githubusercontent.com/NeverSinkDev/NeverSink-Filter/master/NeverSink's%20filter%20-%205-UBER-STRICT.filter"
 url_uber_plus_strict = r"https://raw.githubusercontent.com/NeverSinkDev/NeverSink-Filter/master/NeverSink's%20filter%20-%206-UBER-PLUS-STRICT.filter"
+needs_backup = True
 
 def get_filter_folder(): 
     filter_folder = 'C:\\Users\\'+os.getlogin()+'\\Documents\\My Games\\Path of Exile'
@@ -38,8 +41,7 @@ def get_filter_folder():
                             pass
                         elif (answer == 'N'):
                             print('Exiting the program.')
-                            getpass.getpass('\n\n\nPress ENTER to exit...')
-                            sys.exit()
+                            end_execution()
                         else:
                             print('Please enter only Y or N.')
                             answer = input().upper()
@@ -48,15 +50,20 @@ def get_filter_folder():
                     answer = input().upper()
     else:
         return filter_folder
-    raise Exception('ERROR: This should never happen...')
+    print('ERROR: @get_filter_folder(). This should not happen.')
+    print('Default folder found? '+os.path.exists(filter_folder))
+    print('Folder used: '+filter_folder)
+    print('Please send this to me.')
+    end_execution()
 
 
 def find_filter_type(filter_folder):
     current_filters = []
     for files in os.listdir(filter_folder):
-        if 'NeverSink' in files:
-            filter_type = re.search(r'(\d-.*)\.filter', files)
-            current_filters.append(filter_type.group(1))
+        if '.filter' in files:
+            filter_type = re.search(r'(-|_)(.*)\.filter', files)
+            current_filters.append(filter_type.group(2).upper().replace('_','-').strip())
+    print(current_filters)
     if len(current_filters) == 0:
         print('No NeverSkin filter found, do you wish to download one? (Y/N)')
         answer = input().upper()
@@ -67,6 +74,8 @@ def find_filter_type(filter_folder):
                 print('Please enter only Y or N.')
                 answer = input().upper()
         if (answer.upper() == 'Y'):
+            global needs_backup 
+            needs_backup = False
             print('Choose filter type:')
             print('0-SOFT\n1-REGULAR\n2-SEMI-STRICT\n3-STRICT\n4-VERY-STRICT\n5-UBER-STRICT\n6-UBER-PLUS-STRICT')
             answer = input().upper()
@@ -89,16 +98,38 @@ def find_filter_type(filter_folder):
                     print('Invalid choice, choose again.')
                     answer = input().upper()
         elif (answer.upper() == 'N'):
-            getpass.getpass('\n\n\nPress ENTER to exit...')
-            sys.exit()
+            end_execution()
     else:
         return current_filters
 
-    raise Exception('ERROR: This should never happen...')
+    print('ERROR: @find_filter_type(). This should not happen.')
+    print('Folder used: '+folder)
+    print('Filters found: '+current_filters)
+    print('Please send this to me.')
+    end_execution()
 
 
 def get_filter_version(): #TODO: Find updated and current filter version and compare both
     pass
+
+
+def backup_old_filters(folder):
+    filter_list = []
+    for items in os.listdir(folder):
+        if (".filter" in items):
+            filter_list.append(items)
+    current_date = datetime.now()
+    current_date = current_date.strftime("%y-%m-%d-%H%M%S")
+    os.makedirs(folder+"\\filterBackup"+current_date, exist_ok = True)
+    for items in filter_list:
+        src = os.path.join(folder, items)
+        dest = os.path.join(folder, "filterBackup"+current_date)
+        shutil.copy(src, dest)
+        if (os.path.exists(os.path.join(dest, items))):
+            os.remove(src)
+        else:
+            print('ERROR failed to backup file:' + items)
+    print('Old filters have been backed-up to '+str(os.path.join(folder,"filterBackup"+current_date)))
 
 
 def install_my_custom(): #TODO: If yes, download STRICT filter and add my custom changes to it, also copy .mp3 files into filters folder
@@ -107,29 +138,60 @@ def install_my_custom(): #TODO: If yes, download STRICT filter and add my custom
     pass
 
 
+def end_execution():
+    getpass.getpass('Press ENTER to exit...')
+    sys.exit()
+
+
 def update_filter(filters, folder):
+    updated_at_least_one = False
     for filter_type in filters:    
-        filename = os.path.join(folder,"NeverSink's filter - "+filter_type+".filter") #TODO: change to poe filter folder after testing
+        filename = os.path.join(folder,"NeverSink's filter - "+filter_type+".filter")
         if (filter_type == '0-SOFT'):
             git_file = requests.get(url_soft)
+            updated_at_least_one = True
         elif (filter_type == '1-REGULAR'):
             git_file = requests.get(url_regular)
+            updated_at_least_one = True
         elif (filter_type == '2-SEMI-STRICT'):
             git_file = requests.get(url_semi_strict)
+            updated_at_least_one = True
         elif (filter_type == '3-STRICT'):
             git_file = requests.get(url_strict)
+            updated_at_least_one = True
         elif (filter_type == '4-VERY-STRICT'):
             git_file = requests.get(url_very_strict)
+            updated_at_least_one = True
         elif (filter_type == '5-UBER-STRICT'):
             git_file = requests.get(url_uber_strict)
+            updated_at_least_one = True
         elif (filter_type == '6-UBER-PLUS-STRICT'):
             git_file = requests.get(url_uber_plus_strict)
+            updated_at_least_one = True
         else:
-            raise Exception('ERROR: git_file failed to find correct filter_type, this is probably a error in the find_filter_type() method')
+            pass
+        try:
+            filter_file = open(filename,'wb')
+            filter_file.write(git_file.content)
+        except:
+            print('Error updating filter %s' %filter_type)
+        
+        print('%s has been sucesfully updated' %filter_type)
 
-        filter_file = open(filename,'wb')
-        filter_file.write(git_file.content)
+    if not (updated_at_least_one):
+        print('ERROR: git_file failed to find correct filter_type. No filters where updated.')
+        print('This might happen if your filters are named differently from the default or if they are not a NeveSink filter.')
+        end_execution()
 
+        
+
+
+
+
+
+
+
+#METHOD CALLS=================================================
 folder = get_filter_folder()
 filters = find_filter_type(folder)
 
@@ -144,14 +206,13 @@ while True:
     if (answer == 'Y'):
         break
     if (answer == 'N'):
-        print('Exiting the program.')
-        getpass.getpass('\n\n\nPress ENTER to exit...')
-        sys.exit()
+        end_execution()
     print('Please enter only Y or N.')
     answer = input().upper()
+
+backup_old_filters(folder)
 
 update_filter(filters, folder)
 
 print('\n\n\nUpdate complete.')
-getpass.getpass('Press ENTER to exit...')
-sys.exit()
+end_execution()
